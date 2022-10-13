92675f7da52da7fdb15ff37b455ca731daae1dda
Kody <72269403+kodypistol@users.noreply.github.com>
2022-10-13 16:39:08 +0200

    started styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { add_game } from '../store/actions/actions-types';
+import { HomeStyled } from '../styles/Home.style';
 
 const BRELAN_NB = 6;
 
@@ -9,6 +11,8 @@ function randomNumbers(min, max) {
 
 function Home() {
 
+    const [error, setError] = useState(false);
+
     // lecture du store de la source de vérité read-only
     const { games } = useSelector(state => state);
 
@@ -16,91 +20,94 @@ function Home() {
     const dispatch = useDispatch();
 
     return(
-        <>
-            <h1>Le jeu fou du yam</h1>
+        <HomeStyled>
+            <h1>Jouez au Yam.</h1>
             <p>Le but du jeu est de faire le plus de points possible en faisant des combinaisons de dés.</p>
 
-            <label htmlFor="iterations">Nombre d'expériences</label>
-            <input onChange={
-                (e) => {
-                    // avoid put a string in the store
-                    const value = parseInt(e.target.value);
+            <div className='nb-container'>
+                <label htmlFor="iterations">Nombre d'expériences</label>
+                <input autocomplete="off" type="number" id="iterations" />
+                {error && <p>Le nombre d'expériences doit être un nombre valide. </p>}
+                <button onClick={() => {
 
-                    if (value) {
-                        console.log('dispatching SET_ITERATIONS');
+                    // if input is empty or not a number
+
+                    if(!document.getElementById('iterations').value && isNaN(document.getElementById('iterations').value && document.getElementById('iterations').value < 1)) {
+                        setError(true);
                     }
-                }
-            } type="text" id="iterations" />
+                    else {
+                        setError(false);
 
-            <button onClick={() => {
-                
-                // Déclaration items
-                const iterationsInput = document.getElementById('iterations').value;
+                        // Déclaration items
+                        const iterationsInput = document.getElementById('iterations').value;
 
-                // Structure de données
-                const gameModel = {
-                        brelans: 0,
-                        iterations: [
-                            {
-                                total: 0,
-                                de1: 0,
-                                de2: 0,
-                                de3: 0,
+                        // Structure de données
+                        const gameModel = {
+                                brelans: 0,
+                                iterations: [
+                                    {
+                                        total: 0,
+                                        de1: 0,
+                                        de2: 0,
+                                        de3: 0,
+                                    }
+                                ]
+                        };
+
+                        const iterations = [];
+                        const iterationModel = {
+                            de1: 0,
+                            de2: 0,
+                            de3: 0,
+                            total: 0
+                        };
+                        let totalBrelans = 0;
+
+                        if (iterationsInput) {
+
+                            for (let i = 0; i < iterationsInput; i++) {
+
+                                const dice1 = randomNumbers(1, 6);
+                                const dice2 = randomNumbers(1, 6);
+                                const dice3 = randomNumbers(1, 6);
+
+                                const total = dice1 + dice2 + dice3;
+
+                                iterations.push({
+                                    ...iterationModel,
+                                    de1: dice1,
+                                    de2: dice2,
+                                    de3: dice3,
+                                    total
+                                });
+
+                                // Si on a un brelan 6
+                                if (total === BRELAN_NB * 3) {
+                                    totalBrelans++;
+                                }
                             }
-                        ]
-                };
 
-                const iterations = [];
-                const iterationModel = {
-                    de1: 0,
-                    de2: 0,
-                    de3: 0,
-                    total: 0
-                };
-                let totalBrelans = 0;
+                            // Construction du jeu
+                            const game = {
+                                ...gameModel,
+                                brelans: totalBrelans,
+                                iterations
+                            };
 
-                if (iterationsInput) {
+                            dispatch(add_game({type: 'ADD_GAME', game}));
 
-                    for (let i = 0; i < iterationsInput; i++) {
+                            // log du store
+                            console.log(games);
 
-                        const dice1 = randomNumbers(1, 6);
-                        const dice2 = randomNumbers(1, 6);
-                        const dice3 = randomNumbers(1, 6);
-
-                        const total = dice1 + dice2 + dice3;
-
-                        iterations.push({
-                            ...iterationModel,
-                            de1: dice1,
-                            de2: dice2,
-                            de3: dice3,
-                            total
-                        });
-
-                        // Si on a un brelan 6
-                        if (total === BRELAN_NB * 3) {
-                            totalBrelans++;
                         }
                     }
 
-                    // Construction du jeu
-                    const game = {
-                        ...gameModel,
-                        brelans: totalBrelans,
-                        iterations
-                    };
-
-                    dispatch(add_game({type: 'ADD_GAME', game}));
-
-                    // log du store
-                    console.log(games);
-
-                }
-
-            }}>Lancer les dés</button>
+                }}>Jouer</button>
+            </div>
+            
 
         
-        </>
+        </HomeStyled>
     );
 }
 
---
+++ src/styles/Home.style.jsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const HomeStyled = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+
+    > h1 {
+        font-size: 48px;
+        font-weight: 500;
+    }
+
+    > p {
+        color: #7E7E7E;
+        width: 30%;
+    }
+
+    > .nb-container {
+        display: flex;
+        flex-direction: column;
+        margin-top: 20px;
+        width: 30%;
+        padding: 16px 24px;
+        background-color: #1F1F21;
+        border-radius: 20px;
+
+        > label {
+            font-weight: 500;
+        }
+
+        > input {
+            margin-top: 10px;
+            padding: 10px;
+            border: 1px solid #7E7E7E;
+            border-radius: 5px;
+            transition: all 0.3s ease-in-out;
+
+            &.focus {
+                outline: none;
+                border: 1px solid #4490EB;
+            }
+        }
+
+        > button {
+            margin-top: 10px;
+            padding: 10px;
+            border: 1px solid #7E7E7E;
+            border-radius: 5px;
+            background-color: #7E7E7E;
+            color: white;
+            font-weight: 500;
+            cursor: pointer;
+        }
+    }
+
+
+`;
